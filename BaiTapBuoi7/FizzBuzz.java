5df93ec49e7260b5de8c4f4c0f3ac95a9e9b8c29
manhhhhhhh <125013750+manhhhhhhh@users.noreply.github.com>
2023-05-30 10:53:32 +0700

    Add files via upload

---
+++ BaiTapBuoi7/FizzBuzz.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzz {
+    public static String fizzBuzz(int number) {
+        if (number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
+
+        if (number % 3 == 0)
+            return "Fizz";
+
+        if (number % 5 == 0)
+            return "Buzz";
+
+        return number + "";
+    }
+}
+class FizzBuzzTest {
+
+        @Test
+        void testFizz3() {
+            int number = 3;
+            String expected = "Fizz";
+
+            String result = FizzBuzz.fizzBuzz(number);
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void testFizz6() {
+            int number = 6;
+            String expected = "Fizz";
+
+            String result = FizzBuzz.fizzBuzz(number);
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void testBuzz5() {
+            int number = 5;
+            String expected = "Buzz";
+
+            String result = FizzBuzz.fizzBuzz(number);
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void testBuzz10() {
+            int number = 10;
+            String expected = "Buzz";
+
+            String result = FizzBuzz.fizzBuzz(number);
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void testFizzBuzz15() {
+            int number = 15;
+            String expected = "FizzBuzz";
+
+            String result = FizzBuzz.fizzBuzz(number);
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void testFizzBuzz30() {
+            int number = 30;
+            String expected = "FizzBuzz";
+
+            String result = FizzBuzz.fizzBuzz(number);
+            assertEquals(expected, result);
+        }
+}
